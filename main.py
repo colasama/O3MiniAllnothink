439a2e30fb170bc2488235f578e6a1dde0fe77e0
PlaneCN <lubin2011@live.cn>
2025-01-27 11:11:51 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,48 +1,26 @@
-from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
-from pkg.plugin.events import *  # 导入事件类
+# coding:utf-8
+from pkg.plugin.models import *
+from pkg.plugin.host import EventContext, PluginHost
 
+#import re
+from . import util
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class MyPlugin(BasePlugin):
+@register(name="URLMasker", description="Mask url to bypass", version="0.1", author="Antlt")
+class URLMaskerPlugin(Plugin):
 
     # 插件加载时触发
-    def __init__(self, host: APIHost):
+    # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
+    def __init__(self, plugin_host: PluginHost):
         pass
 
-    # 异步初始化
-    async def initialize(self):
-        pass
 
-    # 当收到个人消息时触发
-    @handler(PersonNormalMessageReceived)
-    async def person_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
-
-    # 当收到群消息时触发
-    @handler(GroupNormalMessageReceived)
-    async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
+    @on(NormalMessageResponded)
+    def group_normal_message_received(self, event: EventContext, **kwargs):
+        msg = kwargs['response_text']
+        ret = removethink(msg)
+        event.add_return('reply', ret)
+        
 
     # 插件卸载时触发
     def __del__(self):
---
+++ util.py
@@ -0,0 +1,4 @@
+# coding:utf-8
+def removethink(rep:str)->str:
+    nothink = rep.split("</think>")
+    return nothink
