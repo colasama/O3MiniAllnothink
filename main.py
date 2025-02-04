508223a3e3a53b3f16054839ddb70e831149e978
PlaneCN <lubin2011@live.cn>
2025-02-04 15:33:46 +0800

    添加prompt中的思考过滤
    通过修改ctx.event.prompt.content实现对话上下文中的思考过程过滤

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # coding:utf-8
+from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
+from pkg.plugin.events import *
 from pkg.plugin.models import *
 from pkg.plugin.host import EventContext, PluginHost
-
-#import re
+import re
 from . import util
 
 # 注册插件
@@ -13,15 +14,19 @@ class URLMaskerPlugin(Plugin):
     # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
     def __init__(self, plugin_host: PluginHost):
         pass
-
+        
+    @handler(PromptPreProcessing)
+    async def _(self, ctx: EventContext):
+        if len(ctx.event.prompt) != 0:
+            for promptindex,promptcontent in enumerate(ctx.event.prompt):
+                if promptindex % 2 != 0:
+                    ctx.event.prompt[promptindex].content = re.sub(r'<think>[\s\S]*?<\/think>\n\n', '', promptcontent.content)
 
     @on(NormalMessageResponded)
     def group_normal_message_received(self, event: EventContext, **kwargs):
         msg = kwargs['response_text']
         ret = util.removethink(msg)
-        self.ap.logger.debug(ret)
         event.add_return('reply', ret)
-        
 
     # 插件卸载时触发
     def __del__(self):
